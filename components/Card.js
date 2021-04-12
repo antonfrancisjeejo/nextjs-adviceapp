3528b4b85f263a9f1082da959a678e63c7de4b5d
Anton Francis Jeejo <anton13jeejo@gmail.com>
2021-04-13 01:48:06 +0530

    Add files via upload

---
+++ components/Card.js
@@ -0,0 +1,12 @@
+import styles from "./Card.module.css";
+function Card(props) {
+  return (
+    <div className={styles.card}>
+      <h1 className={styles.heading}>{props.advice}</h1>
+      <button className={styles.button} onClick={props.fetchAdvice}>
+        <span>GIVE ME ADVICE!</span>
+      </button>
+    </div>
+  );
+}
+export default Card;
